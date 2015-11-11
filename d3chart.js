90f57de0bce0a8ac006f03b509098b3db2c3edf4
ryan <ryanthesmith@gmail.com>
2015-11-11 11:59:07 -0800

    [update] created update function for canvas, nodes rotate on click

--- d3chart.js
+++ d3chart.js
@@ -1,55 +1,143 @@
+// var d3data = {
+//       title: 'projection1',
+//       priority: 10,
+//       img: './assets/wheel',
+//       children: [
+//         {
+//           title: 'immedia',
+//           url: 'https://www.immedia.xyz',
+//           description: 'your real-time encyclopedia',
+//           priority: 1,
+//         },
+//         {
+//           title: 'koupler',
+//           url: 'https://www.immedia.xyz',
+//           description: 'a meeting site for couples', 
+//           priority: 1.5,
+//         },
+//         {
+//           title: 'deep space',
+//           url: 'https://mtcrushmore.github.io',
+//           description: 'solar system creator', 
+//           priority: 2.5,
+//         },
+//         {
+//           title: 'fudwize',
+//           url: 'https://fudwize.herokuapp.xyz',
+//           description: 'connecting foodbanks and restaurants', 
+//           priority: 1.5,
+//         },
+//         {
+//           title: 'gram',
+//           url: 'https://github.com/mtcrushmore/gram',
+//           description: 'real-time whiteboard and chat', 
+//           priority: 2.5,
+//         },
+//       ]
+// };
+
+/* The data represents each section of the page, with different priorities
+given to more important sections (priorities will help determine node radius size
+later) */
 var d3data = {
       title: 'projection1',
-      priority: 10,
-      img: './assets/wheel',
+      priority: 6,
+      img: './assets/background2.jpg',
       children: [
         {
-          title: 'immedia',
-          url: 'https://www.immedia.xyz',
-          description: 'your real-time encyclopedia',
-          priority: 1,
+          title: 'about',
+          img: './assets/background2.jpg',
+          priority: 10,
+          children: [
+            {
+              title: 'about',
+              img: './assets/background2.jpg',
+              priority: 2,
+            }
+          ]
         },
         {
-          title: 'koupler',
-          url: 'https://www.immedia.xyz',
-          description: 'a meeting site for couples', 
-          priority: 1.5,
+          title: 'contact',
+          img: './assets/background2.jpg',
+          priority: 10,
+          children: [
+            {
+              title: 'contact',
+              img: './assets/background2.jpg',
+              priority: 1.4,
+            }
+          ]
         },
         {
-          title: 'deep space',
-          url: 'https://mtcrushmore.github.io',
-          description: 'solar system creator', 
-          priority: 2.5,
+          title: 'projects',
+          img: './assets/background2.jpg',
+          priority: 10,
+          children: [
+            {
+              title: 'projects',
+              img: './assets/background2.jpg',
+              priority: 1.2,
+            }
+          ]
         },
-        {
-          title: 'fudwize',
-          url: 'https://fudwize.herokuapp.xyz',
-          description: 'connecting foodbanks and restaurants', 
-          priority: 1.5,
-        },
-        {
-          title: 'gram',
-          url: 'https://github.com/mtcrushmore/gram',
-          description: 'real-time whiteboard and chat', 
-          priority: 2.5,
-        },
+        // {
+        //   title: 'blog',
+        //   img: './assets/background2.jpg',
+        //   priority: 10,
+        //   children: [
+        //     {
+        //       title: 'blog',
+        //       img: './assets/background2.jpg',
+        //       priority: 1.5,
+        //     }
+        //   ]
+        // },
+        // {
+        //   title: 'blog',
+        //   img: './assets/background2.jpg',
+        //   priority: 10,
+        //   children: [
+        //     {
+        //       title: 'blog',
+        //       img: './assets/background2.jpg',
+        //       priority: 1.5,
+        //     }
+        //   ]
+        // },
+        // {
+        //   title: 'blog',
+        //   img: './assets/background2.jpg',
+        //   priority: 10,
+        //   children: [
+        //     {
+        //       title: 'blog',
+        //       img: './assets/background2.jpg',
+        //       priority: 1.5,
+        //     }
+        //   ]
+        // },
       ]
 };
+
 var root = d3data;
-var width = 1200;
-var height = 800;
+var width = window.innerWidth;
+var height = window.innerHeight;
 
 var centerX = width/2;
-var centerY = 300;
+var centerY = height/3;
 
-var update = function(root) {
+var rotation = 180;
+
+var update = function(root, rotation) {
+
+  d3.select('svg').remove();
 
   var cluster = d3.layout.cluster()
-    .size([360, centerY - 120])
+    .size([width/4, height/3])
     // .sort(null)
 
   var diagonal = d3.svg.diagonal.radial()
-    .projection(function(d) { return [d.y, d.x/180 * Math.PI]; })
+    .projection(function(d) { return [d.y, d.x/rotation * Math.PI]; })
 
   var svg = d3.select('#d3chart').append('svg:svg')
     .attr({
@@ -84,20 +172,23 @@ var update = function(root) {
     .enter().append('svg:g')
     .attr({
       class: 'node',
-      transform: function(d) { return 'rotate(' + (d.x - 90) + ')translate(' + d.y + ')'; }
+      // transform: function(d) { return 'rotate(' + (d.x - 90) + ')translate(' + d.y + ')'; }
     })
     .on('click', function(d) {
-      toggle;
+      console.log('clicked!')
+      console.log(d)
+      toggle(d);
     })
 
   node.append('svg:circle')
     .attr('r', function(d) { return 1e-6; })
 
   var nodeUpdate = node.transition()
-    .duration(500)
-    .attr('transform', function (d) {
-      return 'translate(' + d.y + ',' + d.x + ')';
-    })
+    .duration(1500)
+    // .attr('transform', function (d) {
+    //   return 'translate(' + d.y + ',' + d.x + ')';
+    // })
+    .attr('transform', function(d) { return 'rotate(' + (d.x - rotation) + ')translate(' + d.y + ')'; })
     .select('circle')
     .attr('r', function(d) { 
       if (d._children) { return 200; }
@@ -106,38 +197,40 @@ var update = function(root) {
     .style('fill', function(d) {
       defs.append('svg:pattern')
         .attr('id', 'tile-img' + d.title)
+        .attr('width', '20')
+        .attr('height', '20')
         .append('svg:image')
         .attr('xlink:href', function() {
-          return d.img;
+          if (d.img) { return d.img; }
         })
         .attr({
           x: 0,
-          y: 0,
-          width: 50,
-          height: 50,
+          y: -50,
+          width: 400,
+          height: 400,
         })
-        return 'url(/#tile-img' + d.title + ')'
+        return 'url(#tile-img' + d.title + ')'
     })
 
-  var nodeExit = node.exit().transition()
-    .duration(500)
-    .attr('transform', function(d) {
-      return 'translate(' + d.parent.y + ',' + d.parent.x + ')';
-    })
+  // var nodeExit = node.exit().transition()
+  //   .duration(500)
+  //   .attr('transform', function(d) {
+  //     return 'translate(' + d.parent.y + ',' + d.parent.x + ')';
+  //   })
 }
 
-update(root);
+update(root, rotation);
 
 function toggle(d) {
-  console.log('click!')
-  if (d.children) {
-    d._children = d.children;
-    d.children = null;
-  }
-  if (d._children) {
-    d.children = d._children;
-    d._children = null;
-  }
-  update(root);
+  // if (d.children) {
+  //   d._children = d.children;
+  //   d.children = null;
+  // }
+  // if (d._children) {
+  //   d.children = d._children;
+  //   d._children = null;
+  // }
+  rotation = rotation - 90
+  update(root, rotation);
 }
 
