bd75314b07246a17248f83b91bdcd71103823bed
ryan <ryanthesmith@gmail.com>
2015-12-04 17:00:23 -0800

    [update] changed the projects section to sliders with more information on each

--- app.js
+++ app.js
@@ -2,11 +2,20 @@ $(document).ready(function() {
   
   new WOW().init();
 
+  $('.bxslider').bxSlider({
+    mode: 'fade',
+    // adaptiveHeight: true,
+    // slideWidth: '500',
+    captions: true
+  });
+
   $('.img-thumbnail').hover(function() {
     $(this).css('opacity', 0.4);
   }, function() {
     $(this).css('opacity', 1);
-  })
+  });
+
+
 })
   
 
