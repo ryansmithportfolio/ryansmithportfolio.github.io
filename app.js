2c3c969088d59348383b0a3652f5ea1f3830034e
ryan <ryanthesmith@gmail.com>
2015-12-06 15:00:05 -0800

    [feature]  added navbar and css animation

--- app.js
+++ app.js
@@ -9,12 +9,33 @@ $(document).ready(function() {
     captions: true
   });
 
-  $('.img-thumbnail').hover(function() {
-    $(this).css('opacity', 0.4);
-  }, function() {
-    $(this).css('opacity', 1);
-  });
+  // $('.open').on('click', function() {
+  //   // var el = document.getElementsByClassName('open');
+  //   // el.className = el.className + ' opened';
+  //   $('.open').addClass('opened');
+  //   console.log('clicked on open')
+  //   // console.log('clicked')
+  // });
 
+  // $('body').on('click', function() {
+  //   $('.open').removeClass('opened');
+  // });
+
+  // $('.cls').on('click', function() {
+  //   $('.open').removeClass('opened');
+  // });
+
+  $('.open').on('click', function(event){
+    $(this).addClass('opened');
+    event.stopPropagation();
+  })
+  $('body').on('click', function(event) {
+    $('.open').removeClass('opened');
+  })
+  $('.cls').on('click', function(event){
+    $('.open').removeClass('opened');
+    event.stopPropagation();
+  });
 
 })
   
